994529c01edbf27bb1741b8064d37300954a9d63
Brent Shaffer <bshafs@gmail.com>
2010-10-23 18:21:09 -0500

    adding sfThemeGeneratorPluginConfiguration class

---
+++ config/sfThemeGeneratorPluginConfiguration.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the symfony package.
+ * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * sfThemeGeneratorPlugin configuration.
+ * 
+ * @package    sfThemeGeneratorPlugin
+ * @subpackage config
+ * @author     Brent Shaffer
+ * @version    SVN: $Id: sfThemeGeneratorPluginConfiguration.class.php
+ */
+class sfThemeGeneratorPluginConfiguration extends sfPluginConfiguration
+{
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
